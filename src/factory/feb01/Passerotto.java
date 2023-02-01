af343316f18c8b6d1bedf6f75bf728f25a327aa6
PietroDanieli <1pie7d@gmail.com>
2023-02-01 15:35:44 +0100

    create class Passerotto

---
+++ src/factory/feb01/Passerotto.java
@@ -0,0 +1,17 @@
+package factory.feb01;
+
+public class Passerotto extends Animale {
+
+	@Override
+	public void verso() {
+		// TODO Auto-generated method stub
+		System.out.println("cip-cip");
+	}
+
+	@Override
+	public void mangia() {
+		// TODO Auto-generated method stub
+		System.out.println("insetti");
+	}
+
+}
