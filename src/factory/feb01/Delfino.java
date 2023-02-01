d258bd2976e192849e6678e243cea29eef711b11
PietroDanieli <1pie7d@gmail.com>
2023-02-01 15:39:04 +0100

    create class Delfino

---
+++ src/factory/feb01/Delfino.java
@@ -0,0 +1,17 @@
+package factory.feb01;
+
+public class Delfino extends Animale {
+
+	@Override
+	public void verso() {
+		// TODO Auto-generated method stub
+		System.out.println("iiiiiiiiiik");
+	}
+
+	@Override
+	public void mangia() {
+		// TODO Auto-generated method stub
+		System.out.println("pesci");
+	}
+
+}
