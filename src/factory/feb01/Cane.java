3b8b241ef35c9a07a7508208b28bbcaafeb4468e
PietroDanieli <1pie7d@gmail.com>
2023-02-01 15:32:53 +0100

    create class Cane

---
+++ src/factory/feb01/Cane.java
@@ -0,0 +1,17 @@
+package factory.feb01;
+
+public class Cane extends Animale {
+
+	@Override
+	public void verso() {
+		// TODO Auto-generated method stub
+		System.out.println("bau");
+	}
+
+	@Override
+	public void mangia() {
+		// TODO Auto-generated method stub
+		System.out.println("di tutto");
+	}
+
+}
