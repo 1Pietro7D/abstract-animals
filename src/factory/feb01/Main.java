c00680cf4c20dc265d7916bcb2b46fde25582aee
PietroDanieli <1pie7d@gmail.com>
2023-02-01 16:43:58 +0100

    main method

--- src/factory/feb01/Main.java
+++ src/factory/feb01/Main.java
@@ -4,7 +4,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Cane cane = new Cane();
+		System.out.println("un cane:");
+        cane.dormi();
+        cane.verso();
+        cane.mangia();
+        cane.swim();
+        
+        Passerotto passerotto = new Passerotto();
+        System.out.println("\nun passerotto:");
+        passerotto.dormi();
+        passerotto.verso();
+        passerotto.mangia();
+        passerotto.fly();
+        
+        Aquila aquila = new Aquila();
+        System.out.println("\nun'aquila:");
+        aquila.dormi();
+        aquila.verso();
+        aquila.mangia();
+        aquila.fly();
 
+        Delfino delfino = new Delfino();
+        System.out.println("\nun delfino:");
+        delfino.dormi();
+        delfino.verso();
+        delfino.mangia();
+        delfino.swim();
+        
+        Gatto gatto = new Gatto();
+        System.out.println("\nun gatto:");
+        gatto.dormi();
+        gatto.verso();
+        gatto.mangia();
 	}
 
 }
