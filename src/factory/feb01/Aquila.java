268bcffeba76947e49bc3c036b1972ab7a7fa284
PietroDanieli <1pie7d@gmail.com>
2023-02-01 15:41:08 +0100

    create class Aquila

---
+++ src/factory/feb01/Aquila.java
@@ -0,0 +1,17 @@
+package factory.feb01;
+
+public class Aquila extends Animale {
+
+	@Override
+	public void verso() {
+		// TODO Auto-generated method stub
+		System.out.println("eeeeeeek");
+	}
+
+	@Override
+	public void mangia() {
+		// TODO Auto-generated method stub
+		System.out.println("carnivoro");
+	}
+
+}
