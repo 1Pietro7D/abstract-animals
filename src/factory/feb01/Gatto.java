9d18659abdff17a1437c2631e98059c6b3690f0d
PietroDanieli <1pie7d@gmail.com>
2023-02-01 15:43:59 +0100

    add class Gatto"

---
+++ src/factory/feb01/Gatto.java
@@ -0,0 +1,23 @@
+package factory.feb01;
+
+public class Gatto extends Animale {
+	
+	@Override
+	public void dormi() {
+		// TODO Auto-generated method stub
+		System.out.println("prrrr");
+	}
+	
+	@Override
+	public void verso() {
+		// TODO Auto-generated method stub
+		System.out.println("miao");
+	}
+
+	@Override
+	public void mangia() {
+		// TODO Auto-generated method stub
+		System.out.println("mangierebbe anche te");
+	}
+
+}
